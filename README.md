4bf8d0e9674c20a1007c70438bd0d9028f11f862
Tyler VanderMate <lordoevil@gmail.com>
2022-03-29 22:59:24 -0500

    Basic html skeleton and styling complete. The Graph button will be replace with event listeners eventually. Need to make sure the number 'r' is greater than zero. Could add sine and cosine later if wanted. Need to work on getting the default scalars for the base vectors nice, but need to be able to graph a polynomial first. Need to create functions to take input and add polynomial term objects to list in memory. Need to create a Taylor Expansion function that stores the taylor expanded list in memory. Need to create function to grapth a given polynomial function. Finally, need to update the math equations in the DOM using MathJax format.

---
+++ README.md
@@ -0,0 +1,4 @@
+This directory is a basic represenation of a Taylor Series exapansion
+
+Ideally, the project will allow you to insert a function and the javascript canvas element
+will be able to show the original equation graphed (maybe add automatic scaling) as well as show the taylor series expanstion to a variable r value.
---
+++ script.js
@@ -0,0 +1,60 @@
+// 801x801 pixels
+graph = document.getElementById('graph');
+ctx = graph.getContext('2d');
+
+// props for the x and y axes
+axes = {
+  center: {
+    x: graph.width / 2,
+    y: graph.height / 2,
+  },
+
+  // with and height of dashed lines in pixels
+  dash_w: 5,
+  dash_h: 2,
+
+  // space between dashes
+  gap: 2,
+
+  // maps one unit in the x or y axes to their respective pixel scales
+  // typically want these to be the same scalar
+  base_x: 2,
+  base_y: 2,
+};
+
+// list of terms built
+builtExpression = [];
+
+// makes terms look nicer IMO
+function createTerm(coefficient, power) {
+  return { coeff: coefficient, pow: power };
+}
+
+function drawAxes() {
+  drawXAxis();
+  drawYAxis();
+}
+
+function drawYAxis() {
+  // move about Y axis
+  for (let i = 0; i < graph.height; i = i + axes.dash_w + axes.gap) {
+    ctx.beginPath();
+    ctx.rect(axes.center.x, i, axes.dash_h, axes.dash_w);
+    ctx.fillStyle = '#000';
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+function drawXAxis() {
+  // move about x axis
+  for (let i = 0; i < graph.width; i = i + axes.dash_w + axes.gap) {
+    ctx.beginPath();
+    ctx.rect(i, axes.center.y, axes.dash_w, axes.dash_h);
+    ctx.fillStyle = '#000';
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+drawAxes();
